a0376d2cad5db97125cddb3f5b97afe2b3e0efca
Leonardo Lima <leoexori@hotmail.com>
2022-12-04 04:02:11 -0300

    feat: create validation of requisition and fix next function on server file

--- backEnd/src/modules/costumers/routes/costumers.routes.ts
+++ backEnd/src/modules/costumers/routes/costumers.routes.ts
@@ -1,12 +1,58 @@
 import { Router } from "express";
 import CostumerController from "../controllers/CostumerController";
+import { celebrate, Joi, Segments } from "celebrate";
 
 const costumerRouter = Router();
 const costumerController = new CostumerController();
 
 costumerRouter.get("/", costumerController.index);
-costumerRouter.post("/", costumerController.create);
-costumerRouter.put("/:id", costumerController.update);
-costumerRouter.delete("/:id", costumerController.delete);
+
+// routes with celebrate validation
+costumerRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      birthday: Joi.date().required().iso(),
+      cpf: Joi.string()
+        .regex(/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/)
+        .required(),
+      rg: Joi.string().required(),
+      phone: Joi.string()
+        .regex(/^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/)
+        .required(),
+    },
+  }),
+  costumerController.create
+);
+
+costumerRouter.put(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      birthday: Joi.date().iso(),
+      cpf: Joi.string().regex(/[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}/),
+      rg: Joi.string(),
+      phone: Joi.string().regex(
+        /^\([1-9]{2}\) (?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}$/
+      ),
+    },
+  }),
+  costumerController.update
+);
+
+costumerRouter.delete(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  costumerController.delete
+);
 
 export default costumerRouter;
--- backEnd/src/shared/http/server.ts
+++ backEnd/src/shared/http/server.ts
@@ -1,28 +1,33 @@
 import "reflect-metadata";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import cors from "cors";
 import routes from "./routes";
 import AppError from "../errors/AppError";
-import "../../shared/typeorm";
+import { errors } from "celebrate";
+import "@shared/typeorm";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use((error: Error, request: Request, response: Response) => {
-  if (error instanceof AppError) {
-    return response.status(error.statusCode).json({
+app.use(errors());
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: "error",
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
       status: "error",
-      message: error.message,
+      message: "Internal Server Error",
     });
   }
-
-  return response.status(500).json({
-    status: "error",
-    message: "Internal Server Error",
-  });
-});
+);
 
 app.listen(3333, () => {
   console.log("server is running on port 3333");
