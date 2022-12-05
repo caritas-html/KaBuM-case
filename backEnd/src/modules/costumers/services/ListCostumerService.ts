6360194fd4730820cc7d2e4b1838ff551e159c57
Leonardo Lima <leoexori@hotmail.com>
2022-12-05 05:38:27 -0300

    feat: users and session of users done

--- backEnd/src/modules/costumers/services/ListCostumerService.ts
+++ backEnd/src/modules/costumers/services/ListCostumerService.ts
@@ -5,7 +5,6 @@ import { CostumerRepository } from "../typeorm/repositories/CostumersRepository"
 class ListCostumerService {
   public async execute(): Promise<Costumer[]> {
     const costumerRepository = getCustomRepository(CostumerRepository);
-
     const costumers = await costumerRepository.find();
 
     return costumers;
---
+++ backEnd/src/modules/users/middlewares/isAuthenticated.ts
@@ -0,0 +1,28 @@
+import AppError from "@shared/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import authConfig from "@config/auth";
+
+function isAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) throw new AppError("Missing JWT Token");
+
+  // split the bearer and token
+  const [_, token] = authHeader.split(" ");
+
+  // verify if the received token is from the application hashed secret
+  try {
+    verify(token, authConfig.jwt.secret);
+
+    return next();
+  } catch {
+    throw new AppError("Invalid JWT Token");
+  }
+}
+
+export default isAuthenticated;
